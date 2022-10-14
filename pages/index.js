619df8c5dda28532d650c2e591c9bb1b9a65d600
luca-ros <lucattparte@gmail.com>
2022-10-14 09:10:11 +0200

    ssr next.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,23 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+// source: https://youtu.be/WAMqFdCFotY?t=407
 
-export default function Home() {
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export async function getServerSideProps() {
+  // fetch external API
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+  // get data back in json
+  const data = await res.json();
+
+  return {
+    props: {
+      todos: data,
+    },
+  };
+}
+
+export default function Home({ todos }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -13,43 +28,27 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Next.js <a href="https://nextjs.org">SSR example </a>
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        {/* Using ternary Operator */}
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        {/* Using question mark after 'todos' prevent error undefined before rendering */}
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        {/* show Loading... while fetch in to page  */}
+        {todos?.leght === 0 ? (
+          <div>Liading Page.... </div>
+        ) : (
+          // Otherwise
+          // show todos & for every single todo render out on screen
+          todos?.map((todo) => (
+            <div key={todo.id}>
+              <p>
+                {todo.id}: {todo.title}
+              </p>
+            </div>
+          ))
+        )}
       </main>
 
       <footer className={styles.footer}>
@@ -58,12 +57,12 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
+  );
 }
